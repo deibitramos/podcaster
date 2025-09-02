34a042a42ecb3a1bc07ea117bb80b7de84a2583e
David Ramos <davidramos.13@outlook.com>
2025-09-02 21:50:07 +0200

    default stale time for queries

--- src/qc.ts
+++ src/qc.ts
@@ -1,3 +1,20 @@
 import { QueryClient } from '@tanstack/react-query';
 
-export const qc = new QueryClient();
+export const qc = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 60, // 1 hour
+    },
+  },
+});
+
+// This code is only for TypeScript
+declare global {
+  interface Window {
+    __TANSTACK_QUERY_CLIENT__:
+    import('@tanstack/query-core').QueryClient;
+  }
+}
+
+// This code is for all users
+window.__TANSTACK_QUERY_CLIENT__ = qc;
